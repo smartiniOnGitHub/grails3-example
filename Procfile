af8f548dcf45ac0856b80184ddb9445bcd2fccb3
Sandro Martini <sandro.martini@gmail.com>
2015-09-28 01:37:49 +0200

    update configuration to make it runnable (using embedded Tomcat) with 'grails run-app' (even with 'grails run-app -Dgrails.env=prod' more aligned with the real test here) and even when generating the war with 'gradle stage' and running it with 'java -Dgrails.env=prod -Dserver.port=8080 -jar libs\grails3-example-0.2.war' (note that in Windows libs\*.war or libs\*.jar doesn't work). Note that environmental variables are read in build.gradle and published so application.yml can read them. Updated Procfile. All this latest stuff has still to be tested at Heroku.

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 # web: cd build ; java $JAVA_OPTS -Dgrails.env=prod -jar ../build/server/jetty-runner-*.jar --expand-war --port $PORT libs/*.war
-web: cd build ; java $JAVA_OPTS -Dgrails.env=prod -jar ../build/server/webapp-runner-*.jar --expand-war --port $PORT libs/*.war
-
+# web: cd build ; java $JAVA_OPTS -Dgrails.env=prod -jar ../build/server/webapp-runner-*.jar --expand-war --port $PORT libs/*.war
+web: cd build ; java $JAVA_OPTS -Dgrails.env=prod -Dserver.port=$PORT -jar libs/*.war
--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,13 +1,35 @@
+// Note that all contents of this file will be merged with application.groovy that wins and overrides application.yml, so remember to remove the same settings in the other (main) file for the same environment, or use only one of these two files
+
 environments {
-  production {
-    dataSource {
-      dbCreate = "update"
-      driverClassName = "org.postgresql.Driver"
-      dialect = org.hibernate.dialect.PostgreSQLDialect
-      uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
-      url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
-      username = uri.userInfo.split(":")[0]
-      password = uri.userInfo.split(":")[1]
+/*
+// ok, but due to a Grails issue it's better to move this setting in application.yml,
+// but find a way to pass the value of the desired env var inside it
+    production {
+        dataSource {
+            dbCreate = "update"
+            driverClassName = "org.postgresql.Driver"
+            dialect = org.hibernate.dialect.PostgreSQLDialect
+            uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+            url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
+            username = uri.userInfo.split(":")[0]
+            password = uri.userInfo.split(":")[1]
+        }
+        grails {
+            mongodb {
+                // use an environment variable containing all the connection string if defined, otherwise a default will be used instead
+                connectionString = System.env.MONGODB_URI ?: "mongodb://127.0.0.1/test"
+                options {
+                    autoConnectRetry = true
+                    connectTimeout   = 300
+                    maxWaitTime      = 120000
+                    socketTimeout    = 0
+                    socketKeepAlive  = false
+                    writeNumer       = 0
+                    writeTimeout     = 0
+                    writeFsync       = false
+                }
+            }
     }
-  }
+ */
+
 }
